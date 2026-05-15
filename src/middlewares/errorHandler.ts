c223c353cee7b778f7c7c480ae4488ba6b3edfa8
Karrar Al-Jawasim <kaljawasim@gmail.com>
2026-05-15 21:38:04 +0300

    feat: add AppError util custom error class , and glubal error handler

---
+++ src/middlewares/errorHandler.ts
@@ -0,0 +1,26 @@
+import type { Request, Response, NextFunction } from "express";
+import { AppError } from "../utils/appError.js";
+
+export const globalErrorHandler = (
+  err: Error,
+  _req: Request,
+  res: Response,
+  _next: NextFunction,
+) => {
+  if (err instanceof AppError && err.isOperational) {
+    res.status(err.statusCode).json({
+      success: false,
+      message: err.message,
+    });
+
+    return;
+  }
+
+  console.error(`Unhandler Error`);
+
+  res.status(500).json({
+    success: false,
+    message: err.message,
+    stack: err.stack,
+  });
+};
---
+++ src/utils/AppError.ts
@@ -0,0 +1,36 @@
+export class AppError extends Error {
+  readonly statusCode: number;
+  readonly isOperational: boolean;
+
+  constructor(message: string, statusCode = 500, isOperational = true) {
+    super(message);
+    this.statusCode = statusCode;
+    this.isOperational = isOperational;
+    Object.setPrototypeOf(this, new.target.prototype);
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+export class NotFoundError extends AppError {
+  constructor(resource = "Resource") {
+    super(`${resource} not found`, 404);
+  }
+}
+
+export class UnauthorizedError extends AppError {
+  constructor(message = "Unauthorized") {
+    super(message, 401);
+  }
+}
+
+export class ConflictError extends AppError {
+  constructor(resource = "Resource") {
+    super(`${resource} already exists`, 409);
+  }
+}
+
+export class ForbiddenError extends AppError {
+  constructor(message = `You don't have permission to access this resource. `) {
+    super(message, 403);
+  }
+}
